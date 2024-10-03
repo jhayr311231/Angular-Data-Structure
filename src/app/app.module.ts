166c1dfdce3288f8d009bd815882982f5cfc6cc4
jhayr311231 <jhayrbuenafe31@gmail.com>
2024-10-04 02:49:37 +0800

    feat(vegetable-list): add vegetable list component with add, edit, and remove functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import { SubjectListComponent } from './components/subject-list/subject-list.com
 import { CountryListComponent } from './components/country-list/country-list.component';
 import { SportsListComponent } from './components/sports-list/sports-list.component';
 import { VegetableListComponent } from './components/vegetable-list/vegetable-list.component';
+import { AnimalListComponent } from './components/animal-list/animal-list.component';
 
 @NgModule({
   declarations: [
@@ -33,6 +34,7 @@ import { VegetableListComponent } from './components/vegetable-list/vegetable-li
     CountryListComponent,
     SportsListComponent,
     VegetableListComponent,
+    AnimalListComponent,
 
     
   ],
---
+++ src/app/components/animal-list/animal-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-animal-list',
+  templateUrl: './animal-list.component.html',
+  styleUrl: './animal-list.component.css'
+})
+export class AnimalListComponent {
+
+}
